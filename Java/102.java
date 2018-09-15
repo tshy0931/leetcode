a358e4b85da7818ac29253b3889a4a0308de6f0a
tshy0931 <>
2018-09-15 15:12:40 +0100

    refine 102

--- Java/102.java
+++ Java/102.java
@@ -1,7 +1,9 @@
 /**
-Use FIFO queue for level-order tree traversal.
-Use a pointer to denote last node of each level,
-and create a new array in result list when visiting last node at current level.
+Use FIFO queue to achieve level-order traversal. Enqueue the root node.
+While the queue is not empty, dequeue a node each time,
+add its value to the list of current level, and enqueue its child nodes.
+To identify when a level has been fully processed, use a TreeNode reference, 'last', to point to the last node of current level.
+When we finished processing the node referenced by 'last', update 'last' to point to the last element in the queue.
 
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,41 +15,24 @@ and create a new array in result list when visiting last node at current level.
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        
         List<List<Integer>> result = new ArrayList<>();
+        List<Integer> level = new ArrayList<>();
+        Deque<TreeNode> queue = new ArrayDeque<>();
         if(root == null) return result;
-        
-        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
         TreeNode last = root;
-        queue.offer(root);
+        queue.offerLast(root);
         
-        List<Integer> currLevel = new ArrayList<>();
-        
-        while(!queue.isEmpty()){
-            TreeNode n = queue.poll();            
-            currLevel.add(n.val);
-
-            if(n.left != null){
-                queue.offer(n.left);
+        while(!queue.isEmpty()) {
+            TreeNode n = queue.pollFirst();
+            level.add(n.val);
+            if(n.left != null) queue.offerLast(n.left);
+            if(n.right != null) queue.offerLast(n.right);
+            if(n == last) {
+                last = queue.peekLast();
+                result.add(level);
+                level = new ArrayList<>();
             }
-            if(n.right != null){
-                queue.offer(n.right);
-            }
-                        
-            if(last == n){
-                if(n.left == null && n.right == null){
-                    last = queue.peekLast();
-                }else if(n.right != null){
-                    last = n.right;
-                }else if(n.left != null){
-                    last = n.left;
-                }
-
-                result.add(currLevel);
-                currLevel = new ArrayList<>();
-            }            
         }
-        
         return result;
     }
 }
